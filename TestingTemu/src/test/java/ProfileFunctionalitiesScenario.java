827346d3067c9f4ab3c678db48ae3ebd03b94f94
Eldar <eldar.satara@stu.ibu.edu.ba>
2025-12-12 20:06:21 +0100

    Preparation project initialization

---
+++ TestingTemu/src/test/java/ProfileFunctionalitiesScenario.java
@@ -0,0 +1,2 @@
+public class ProfileFunctionalitiesScenario {
+}
---
+++ TestingTemu/src/test/java/SeleniumSetupClass.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+// This file is supposed to provide a brief overview of the Selenium setup and code that shall be used in all the other classes
+
+public class SeleniumSetupClass {
+
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    // How to setup the connection to the webpage and find the Chrome driver
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/user/selenium/chromedriver-mac-arm64/chromedriver"); // specify the path to chromedriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.example.com"; //Specify the webpage
+    }
+
+    //How to close the connection after every test scenario
+    @AfterAll
+    public static void tearDown() {
+        // Close the browser
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+
+}
